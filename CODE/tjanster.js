016380f20ddd535518a3e06d7a09ed41d227c8e2
f3ras78 <113246173+f3ras78@users.noreply.github.com>
2022-09-21 00:07:58 +0200

    f

---
+++ CODE/tjanster.js
@@ -0,0 +1,15 @@
+Vue.createApp({
+    data() {
+      return {
+        total: 0,
+        counter: 0
+        };
+      },
+      methods: {
+        clear(i) {
+          
+        }
+
+      
+    }
+  }).mount('#app');
