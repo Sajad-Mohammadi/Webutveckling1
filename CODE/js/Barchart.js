1bc75bdf9acf5bb594ed2960f6535b89ef6bf7d3
Sajjad <105199844+Sajad-Mohammadi@users.noreply.github.com>
2022-09-28 23:54:25 +0200

    SUUIIIII

--- CODE/js/Barchart.js
+++
@@ -1,7 +0,0 @@
-(function(document){
-   var _bars = [].slice.call(document.querySelectorAll('.bar-inner'));
-
-   _bars.map(function(bar, index){
-    bar.style.width = bar.dataset.procent
-   })
-})(document);
